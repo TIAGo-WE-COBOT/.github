7d967def78044ee591be8bb0cb0e55253762664e
Luca <58377048+Luca-Pozzi@users.noreply.github.com>
2023-03-22 14:30:11 +0100

    add hri_setup

---
+++ hri_ws_setup.bash
@@ -0,0 +1,4 @@
+# Set an environment variable to point to the virtual environment 
+# with the modules needed to run the ROS4HRI nodes 
+export HRI_VENV="$HOME/hri_ws/hri_venv/bin/python3"
+source devel/setup.bash
