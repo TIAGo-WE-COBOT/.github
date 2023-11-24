8311ba9df99bdff34ce2b1ce52605600ec80dab6
Luca <58377048+Luca-Pozzi@users.noreply.github.com>
2023-11-24 10:29:09 +0100

    move bash files to tiago_tools repo

--- hri_ws_setup.bash
+++
@@ -1,4 +0,0 @@
-# Set an environment variable to point to the virtual environment 
-# with the modules needed to run the ROS4HRI nodes 
-export HRI_VENV="$HOME/hri_ws/hri_venv/bin/python3"
-source devel/setup.bash
